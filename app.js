2088a471089dc0042bdac4fcc2d805a875047ead
famesensor <famfew25@gmail.com>
2020-01-24 00:25:49 +0700

    Update app.js, Add passport, admin, profile

--- app.js
+++ app.js
@@ -1,7 +1,37 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+
+const admin = require('./routes/admin');
+const post = require('./routes/post');
+const profile = require('./routes/profile');
+const user = require('./routes/user');
 
 //init app 
 const app = express();
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
+// DB config
+const db = require('./config/db.mongodb');
+
+// Connect to MongoDB
+
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport Config
+require('./config/passport')(passport);
+
+// Use Routes
+app.use('/api/admin', admin);
+app.use('/api/post', post);
+app.use('/api/profile', profile);
+app.use('/api/user', user);
 
 // Set portnumber
 const portnumber = process.env.PORT || 3000
