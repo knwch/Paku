7ea13dc16a45fb94e872094c0d564b5edab814fa
knwch <t.korrawich@protonmail.com>
2020-02-01 21:17:02 +0700

    update login form

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Responsive, Container } from 'semantic-ui-react';
-import LoginForm from '../components/forms/loginforms/LoginForm';
+// import LoginForm from '../components/forms/loginforms/LoginForm';
+import SimpleReactValidator from 'simple-react-validator';
+import { Responsive, Container, Icon, Input, Button, Form, Label, Grid } from 'semantic-ui-react';
 
 class Login extends Component {
 
@@ -10,27 +11,67 @@ class Login extends Component {
       email: '',
       password: ''
     };
+
+    this.validator = new SimpleReactValidator({
+      element: message => <Label basic color='red' pointing>{message}</Label>,
+      messages: {
+        required: 'โปรดระบุ:attribute',
+      }
+    });
   }
 
   handleChange = input => e => {
     this.setState({ [input]: e.target.value });
   };
 
-  render() {
-    const { username,
-      password } = this.state;
-    const values =
-    {
-      username,
-      password
-    };
+  continue = e => {
+    if (this.validator.allValid()) {
+      e.preventDefault();
+    } else {
+      this.validator.showMessages();
+      // rerender to show messages for the first time
+      // you can use the autoForceUpdate option to do this automatically`
+      this.forceUpdate();
+    }
+  };
 
+  render() {
     return (
       <Responsive>
         <Container fluid>
-          <LoginForm
-            handleChange={this.handleChange}
-            values={values} />
+          <Grid className='mb-4' centered>
+            <Grid.Column mobile={14} tablet={7} computer={6}>
+              <h4 className="text-center mb-4">เข้าสู่ระบบ</h4>
+              <Form>
+
+                <Form.Field className="text-left">
+                  <Input fluid iconPosition='left' placeholder='ชื่อผู้ใช้'>
+                    <Icon name='user' />
+                    <input type="text" className="form-control" onChange={this.handleChange('username')} defaultValue={this.state.username} />
+                  </Input>
+                  {this.validator.message('ชื่อผู้ใช้', this.state.username, 'required')}
+                </Form.Field>
+
+                <Form.Field className="text-left">
+                  <Input fluid iconPosition='left' placeholder='รหัสผ่าน'>
+                    <Icon name='lock' />
+                    <input type="text" className="form-control" onChange={this.handleChange('password')} defaultValue={this.state.password} />
+                  </Input>
+                  {this.validator.message('รหัสผ่าน', this.state.password, 'required')}
+                </Form.Field>
+
+                <div className='text-center'>
+                  <Button onClick={this.continue} className='btn-paku' color='yellow' animated>
+                    <Button.Content visible>เข้าสู่ระบบ</Button.Content>
+                    <Button.Content hidden>
+                      <Icon name='arrow right' />
+                    </Button.Content>
+                  </Button>
+                </div>
+
+              </Form>
+            </Grid.Column>
+          </Grid>
         </Container>
       </Responsive>
     );
