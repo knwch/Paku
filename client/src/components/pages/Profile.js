8625dc29290df8f4a10e53de4babd0ba2d2b6a98
knwch <t.korrawich@protonmail.com>
2020-02-07 00:45:46 +0700

    move card components to profile page

--- client/src/components/pages/Profile.js
+++ client/src/components/pages/Profile.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import ProfileDetailCard from '../cards/profile/ProfileDetailCard';
-import ContactCard from '../cards/profile/ContactCard';
+import RecommendCard from '../cards/RecommendCard';
+import { Card, Icon, Input, Divider, Button, Image, Modal, Header, Grid, Container, Responsive, Segment } from 'semantic-ui-react';
 
 class Profile extends Component {
-    componentDidMount(){
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: "Name Name Parking",
+            rate: "4.93",
+            bio: "asdfasdfasdfasdfasdf",
+            location: "กรุงเทพมหานคร",
+            telephone: "0915466421",
+            email: "ccc",
+        };
+    }
+
+    componentDidMount() {
         document.title = "🐤 Profile"
     }
 
@@ -13,15 +26,68 @@ class Profile extends Component {
             <div className="container-fluid">
                 <div className="row">
                     <div className="col-md-4">
-                        <ContactCard />
+                        <Card fluid>
+                            <Card.Content>
+                                <Image src={require('../imgs/Logo.png')} size='small' centered wrapped />
+                                <Divider />
+                                <Card.Description textAlign='left' className='pb-1'>
+                                    <Icon name='yellow star' />
+                                    4.59
+                                </Card.Description>
+                                <Card.Description textAlign='left' className='pb-1'>
+                                    <Icon name='phone' flipped='horizontally' />
+                                    {this.state.telephone}
+                                </Card.Description>
+                                <Card.Description textAlign='left' className='pb-1'>
+                                    <Icon name='mail' />
+                                    abcd@mail.co
+                                </Card.Description>
+                            </Card.Content>
+                        </Card>
                     </div>
                     <div className="col-md-8">
-                        <ProfileDetailCard />
+                        <Card fluid>
+                            <Card.Content>
+                                <Card.Header textAlign='left'>{this.state.name}
+                                    <Button basic circular icon='edit outline' floated='right'></Button>
+                                </Card.Header>
+                                <Card.Description textAlign='left'>
+                                    {this.state.bio}
+                                </Card.Description>
+                                <Divider />
+                                <RecommendCardList />
+                            </Card.Content>
+                        </Card>
                     </div>
                 </div>
             </div>
         );
     }
+}
+
+function RecommendCardList() {
+    return (
+        <div>
+            <Card.Description textAlign='left' className='pb-1'>
+                ที่จอดรถของฉัน
+            </Card.Description>
+            <Card.Description>
+                <Grid textAlign='center' stackable columns={3}>
+                    <Grid.Row>
+                        <Grid.Column>
+                            <RecommendCard />
+                        </Grid.Column>
+                        <Grid.Column>
+                            <RecommendCard />
+                        </Grid.Column>
+                        <Grid.Column>
+                            <RecommendCard />
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+            </Card.Description>
+        </div>
+    );
 }
 
 export default Profile;
