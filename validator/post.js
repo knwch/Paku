f28af0ed009aebd3016257896b374e628eaf612a
famesensor <famfew25@gmail.com>
2020-02-20 00:16:14 +0700

    Update validator post

--- validator/post.js
+++ validator/post.js
@@ -5,9 +5,15 @@ module.exports = validatePostInput = (data) => {
     let errors = {};
 
     data.title = !isEmpty(data.title) ? data.title : '';
-    data.detail = !isEmpty(data.detail) ? data.detail : '';
-    data.explanation = !isEmpty(data.explanation) ? data.explanation : '';
-    data.date = !isEmpty(data.date) ? data.date : '';
+    data.typeofpark = !isEmpty(data.typeofpark) ? data.typeofpark : '';
+    data.numberofcar = !isEmpty(data.numberofcar) ? data.numberofcar : '';
+    data.typeofcar = !isEmpty(data.typeofcar) ? data.typeofcar : '';
+    data.explain = !isEmpty(data.explain) ? data.explain : '';
+    data.address = !isEmpty(data.address) ? data.address : '';
+    data.longitude = !isEmpty(data.longitude) ? data.longitude : '';
+    data.latitude = !isEmpty(data.latitude) ? data.latitude : '';
+    data.open = !isEmpty(data.open) ? data.open : '';
+    data.close = !isEmpty(data.close) ? data.close : '';
     data.price = !isEmpty(data.price) ? data.price : '';
 
     if (!Validator.isLength(data.title, {min : 6 , max : 100})) {
@@ -16,32 +22,35 @@ module.exports = validatePostInput = (data) => {
     if (Validator.isEmpty(data.title)) {
         errors.title = `Title field is required`;
     }
-    if (Validator.isEmpty(data.detail.point)) {
-        errors.point = `Point field is required`;
-    }
-    if (Validator.isEmpty(data.detail.typeofpark)) {
+    if (Validator.isEmpty(data.typeofpark)) {
         errors.typeofpark = `Type field is required`;
     }
-    if (Validator.isEmpty(data.detail.numberofcar)) {
-        errors.numberofcar = `Number of car is required`;
-    }
-    if (!Validator.isNumeric(data.detail.numberofcar)) {
+    if (!Validator.isNumeric(data.numberofcar)) {
         errors.numberofcar = `Number of car must be Integer`;
     }
-    if (Validator.isEmpty(data.detail.typeofcar)) {
+    if (Validator.isEmpty(data.numberofcar)) {
+        errors.numberofcar = `Number of car is required`;
+    }
+    if (Validator.isEmpty(data.typeofcar)) {
         errors.typeofcar = `Type car is required`;
     }
-    if (Validator.isEmpty(data.date.open)) {
+    if (Validator.isEmpty(data.explain)) {
+        errors.explain = `Explain is required`;
+    }
+    if (Validator.isEmpty(data.address)) {
+        errors.address = `Address is required`;
+    }
+    if (Validator.isEmpty(data.longitude)) {
+        errors.map = `You must fix point in map`;
+    }
+    if (Validator.isEmpty(data.latitude)) {
+        errors.map = `You must fix point in map`;
+    }
+    if (Validator.isEmpty(data.open)) {
         errors.open = `Opne field is required`;
     }
-    if (Validator.isEmpty(data.date.close)) {
+    if (Validator.isEmpty(data.close)) {
         errors.close = `Close field is required`;
-    }
-    if (Validator.isEmpty(data.explanation.about)) {
-        errors.about = `About field is required`;
-    }
-    if (!Validator.isLength(data.explanation.about, {min : 6 , max : 300})) {
-        errors.about = `About must be between 6 and 300 characters`;
     }
     if (Validator.isEmpty(data.price)) {
         errors.price = `Price field is required`;
