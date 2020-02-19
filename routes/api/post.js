97f66df1ae3564c1f8dd6efd0627bb37dfb22a2d
famesensor <famfew25@gmail.com>
2020-02-20 00:15:49 +0700

    Add path

--- routes/api/post.js
+++ routes/api/post.js
@@ -9,20 +9,6 @@ const Post = require('../../models/post');
 // import input validation
 const validatePostInput = require('../../validator/post');
 
-// authenticateIdCard = (req, res) => {
-//     User.findById(req.user.id).where('Card.confirm', true)
-//         .then((user) => {
-//             if (!user) {
-//                 return res.json({ user: "User must comfirm CardID"})
-//             }
-//             // res.send(user)
-//             next();
-//         })
-//         .catch((err) => {
-//             res.json(err);
-//         });
-// }
-
 // @route   GET api/post
 // @desc    Default Route
 // @access  Public
@@ -61,20 +47,77 @@ router.get('/handle/:postId', (req, res) => {
 // @desc    Post park my self
 // @access  Private
 router.post('/addPost', passport.authenticate('jwt', { session: false }), (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+    User.findById(req.user.id).where('Card.confirm', true)
+        .then((user) => {
+            if (!user) {
+                errors.user = 'User must comfirm CardID'
+                return res.json(errors)
+            }
 
+
+            if (!isValid) {
+                console.log(errors)
+                return res.status(400).json(errors);
+            }
+
+            const newPost = new Post({
+                title: req.body.title,
+                detail: {
+                    typeofpark: req.body.typeofpark,
+                    numberofcar: req.body.numberofcar,
+                    typeofcar: req.body.typeofcar,
+                    explain: req.body.explain,
+                    rule: req.body.rule,
+                    nearby: req.body.nearby,
+                    facility: req.body.facility
+                },
+                location: {
+                    address: req.body.address,
+                    longitude: req.body.longitude,
+                    latitude: req.body.latitude
+                },
+                date: {
+                    open: req.body.open,
+                    close: req.body.close
+                },
+                user: req.user.id,
+                price: req.body.price
+            })
+
+            newPost.save()
+                .then((post) => {
+                    res.json(post);
+                })
+        })
+        .catch((err) => {
+            res.json(err);
+        });
 });
 
 // @route   DELETE api/post/delete/:postId
 // @desc    Delete park my self
 // @access  Private
-router.delete('/delete/:postId', passport.authenticate('jwt', { session: false }), (req, res) => {
+router.delete('/delete/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+    Post.findById(req.params.id)
+        .then((post) => {
+            if (post.user.toString() !== req.user.id) {
+                return res.status(401).json({ post : "User not authorized" });
+            }
 
+            post.remove().then((post) => {
+                res.json({ post: "Success"});
+            })
+        })
+        .catch((err) => {
+            res.json({ post: "Post not found "});
+        })
 });
 
 // @route   POST api/post/edit/:postId
 // @desc    Edit info park my self
 // @access  Private
-router.post('edit/:postId', passport.authenticate('jwt', { session: false }), (req, res) => {
+router.post('edit/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
 
 });
 
