65d1956bbeca795916b18bf61287f444d4e2540a
famesensor <famfew25@gmail.com>
2020-02-27 00:07:05 +0700

    Disconnect mysql

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const admin = require('./routes/api/admin');
 const post = require('./routes/api/post');
 const profile = require('./routes/api/profile');
 const user = require('./routes/api/user');
-const book = require('./routes/api/booking');
-const mysqlConnection = require('./controller/mysqlConnect');
+// const book = require('./routes/api/booking');
+// const mysqlConnection = require('./controller/mysqlConnect');
 
 //init app 
 const app = express();
@@ -37,7 +37,7 @@ app.use('/api/admin', admin);
 app.use('/api/posts', post);
 app.use('/api/profile', profile);
 app.use('/api/users', user);
-app.use('/api/book', book);
+// app.use('/api/book', book);
 
 // Set portnumber
 const portnumber = process.env.PORT || 5000
