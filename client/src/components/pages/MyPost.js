4c3a861f30b0c42a02adf785c3508fd29e2c266c
knwch <knwch@harmoinc.local>
2020-03-05 01:54:04 +0700

    add myPost

---
+++ client/src/components/pages/MyPost.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import { Responsive, Container, Button, Grid, Header, Divider, Transition, Image } from 'semantic-ui-react';
+import { getPosts } from '../../redux/actions/postActions';
+import { connect } from 'react-redux';
+import { withRouter, Redirect } from 'react-router-dom';
+import { storage } from '../../config/firebase-config';
+
+class MyPost extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      title: '',
+      errors: {}
+    };
+  }
+
+  componentDidMount() {
+    document.title = 'Paku - Login';
+    this.props.getPosts();
+  }
+
+  componentWillReceiveProps(nextProps) {
+
+    const posts = nextProps.post.posts;
+    const user = nextProps.auth.user;
+    const postFind = posts.find((val) => val.user === user.id)
+
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
+
+    if (postFind != null) {
+      this.setState({
+        title: postFind.title
+      })
+    }
+
+
+  }
+
+
+  render() {
+    return (
+      <Responsive>
+        <Container fluid>
+          <Grid className='mb-4'>
+            <Grid.Column mobile={16} tablet={10} computer={10}>
+
+                {/* <div className='img-center-square'>
+                  <Image
+                    src='https://react.semantic-ui.com/images/wireframe/image.png'
+                    wrapped
+                    ui={false}
+                  />
+                </div> */}
+
+                {this.state.title}
+
+                <Button basic>
+                  <Button.Content visible>พักชั่วคราว</Button.Content>
+                </Button>
+
+                <Button basic>
+                  <Button.Content visible>ลบ</Button.Content>
+                </Button>
+
+                <Button basic>
+                  <Button.Content visible>แก้ไข</Button.Content>
+                </Button>
+
+            </Grid.Column>
+          </Grid>
+        </Container>
+      </Responsive>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  errors: state.errors,
+  post: state.post,
+  auth: state.auth
+})
+
+export default connect(mapStateToProps, { getPosts })(withRouter(MyPost));
