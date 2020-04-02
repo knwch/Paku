8539fd30e07e804a729107617598fec86e258d5c
famesensor <famfew25@gmail.com>
2020-04-02 22:04:38 +0700

    :sparkles: Add function

--- controller/admin.js
+++ controller/admin.js
@@ -1,10 +1,15 @@
-const Admin = require('../models/admin')
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const key = require('../config/db.mongodb');
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const key = require('../config/db.mongodb')
 
 // import input validation
-const validateLoginInput = require('../validator/login');
+const validateLoginInput = require('../validator/login')
+
+const Admin = require('../models/admin')
+const User = require('../models/user')
+const Post = require('../models/post')
+
+const errors = {}
 
 exports.register = (req, res) => {
     const errors = {}
@@ -29,16 +34,16 @@ exports.register = (req, res) => {
 
             bcrypt.genSalt(10, (err, salt) => {
                 bcrypt.hash(newAdmin.password, salt, (err, hash) => {
-                    if (err) throw err;
-                    newAdmin.password = hash;
+                    if (err) throw err
+                    newAdmin.password = hash
                     newAdmin.save()
                         .then((admin) => {
-                            res.json(admin);
+                            res.json(admin)
                             // console.log(user);
                         })
                         .catch((err) => {
-                            console.log(err);
-                        });
+                            console.log(err)
+                        })
                 })
             })
         })
@@ -85,3 +90,87 @@ exports.login = (req, res) => {
         })
     })
 }   
+
+exports.userall = (req, res) => {
+    User.find()
+        .then((user) => {
+            if (user.length === 0) {
+                errors.user = `No have user`
+                return res.status(200).json(errors);
+            }
+            res.json(user)
+        })
+        .catch((err) => {
+            errors.user = `Server errors`
+            res.json(errors)
+        })
+}
+
+exports.userConfirm = (req, res) => {
+    User.find({Card:{confirm: false}})
+        .then((user) => {
+            if (user.length === 0) {
+                errors.user = `No have user`
+                return res.status(200).json(errors);
+            }
+            res.json(user)
+        })
+        .catch((err) => {
+            errors.user = `No have user`
+            res.json(errors)
+        })
+}
+
+exports.userById = (req, res) => {
+    User.findById(req.params.id)
+        .then((user) => {
+            res.json(user)
+        })
+        .catch((err) => {
+            errors.user = `No User found with that ID`
+            res.json(errors)
+        })
+}
+
+exports.confirmUser = (req, res) => {
+    User.findById(req.params.id)
+        .then((user) => {
+            if (user.Card.idCard === 0 || user.Card.laser === "") {
+                errors.user = `Idcard is not empty`
+                return res.status(400).json(errors)
+            }
+            user.Card = {
+                confirm: true
+            }
+            user.save().then((user) => res.json(user))
+        })
+        .catch((err) => {
+            errors.user = `No User found with that ID`
+            res.json(errors)
+        })
+}
+
+exports.unConfirmUser = (req, res) => {
+    User.findById(req.params.id)
+        .then((user) => {
+            user.Card = {
+                idCard: 0,
+                laser: "",
+                confirm: false
+            }
+            user.save().then((user) => res.json({ success: true }))
+        })
+        .catch((err) => {
+            errors.user = `No User found with that ID`
+            res.json(errors)
+        })
+}
+
+exports.delUser = (req, res) => {
+    User.findByIdAndDelete({ _id: req.user.id }).then(
+        Post.findByIdAndDelete({ user: req.user.id })
+            .then(res.json({ success: true }))
+            .catch((err) => res.json(err))
+    )
+    .catch((err) => (err) => res.json(err))
+}
