6a220a0aabfb38dafb4ac50a1960bafbb555cc9a
famesensor <famfew25@gmail.com>
2020-04-03 23:15:51 +0700

    :fire: del pasport admin

--- config/passport.js
+++ config/passport.js
@@ -24,17 +24,17 @@ module.exports = (passport) => {
     }))
 }
 
-module.exports = (passportAdmin) => {
-    passportAdmin.use(new JwtStrategy(opts, (jwt_payload, done) => {
-        Admin.findById(jwt_payload.id)
-        .then((admin) => {
-            if (admin) {
-                return done(null, admin);
-            }
-            return done(null, false);
-        })
-        .catch((err) => {
-            console.log(err);
-        })
-    }))
-}
+// module.exports = (passportAdmin) => {
+//     passportAdmin.use(new JwtStrategy(opts, (jwt_payload, done) => {
+//         Admin.findById(jwt_payload.id)
+//         .then((admin) => {
+//             if (admin) {
+//                 return done(null, admin);
+//             }
+//             return done(null, false);
+//         })
+//         .catch((err) => {
+//             console.log(err);
+//         })
+//     }))
+// }
