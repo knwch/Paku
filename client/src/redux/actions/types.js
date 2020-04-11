b6e93311851eb0b5bcd21bdcf27d1ad1ab955c4d
famesensor <famfew25@gmail.com>
2020-04-11 22:05:12 +0700

    :sparkles: add type

--- client/src/redux/actions/types.js
+++ client/src/redux/actions/types.js
@@ -11,6 +11,7 @@ export const GET_PROFILES = 'GET_PROFILES';
 export const POST_LOADING = 'POST_LOADING';
 export const GET_POSTS = 'GET_POSTS';
 export const GET_POST = 'GET_POST';
+export const SEARCH_POST = 'SEARCH_POST'
 export const ADD_POST = 'ADD_POST';
 export const DELETE_POST = 'DELETE_POST';
 export const AVAILABLE_POST = 'AVAILABLE_POST';
