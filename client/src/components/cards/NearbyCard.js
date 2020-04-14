e162eb033c97cde69a4d2e9545cb81fc469a0d00
knwch <knwch@harmoinc.local>
2020-04-14 22:51:11 +0700

    modified card props

--- client/src/components/cards/NearbyCard.js
+++ client/src/components/cards/NearbyCard.js
@@ -1,23 +1,42 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Card, Icon, Image } from "semantic-ui-react";
 
 class NearbyCard extends Component {
-    render() {
-        return (
-            <div className="Card">
-                <div className="card mb-4 border-0">
-                    <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-                    <div className="card-body text-left p-0 pt-2">
-                        <div className="d-flex justify-content-between align-items-center">
-                            <text className="card-text font-weight-bold">Parking Name</text>
-                            <small className="text-muted">4.93</small>
-                        </div>
-                        <small className="card-text">Bangkok</small><br></br>
-                        <small className="card-text font-weight-bold">120 / HOUR</small>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <Card.Group className="mb-4 link" centered>
+        <Card
+          href={this.props.url}
+          link
+          className="mb-0 text-decoration-none"
+          fluid
+        >
+          {/* <div className="img-center-180"> */}
+          <Image src={this.props.photo} wrapped ui={false} />
+          {/* </div> */}
+          <Card.Content>
+            <Card.Description
+              textAlign="left"
+              className="d-flex justify-content-between align-items-center pt-2 pb-0"
+            >
+              <h5>
+                <div>{this.props.title}</div>
+              </h5>
+              <small>
+                <Icon name="star" color="yellow" /> {this.props.rate.toFixed(1)}
+              </small>
+            </Card.Description>
+            <Card.Description textAlign="left" className="pb-1">
+              กรุงเทพมหานคร
+            </Card.Description>
+            <Card.Description textAlign="left" className="pb-1">
+              {this.props.price}/ชั่วโมง
+            </Card.Description>
+          </Card.Content>
+        </Card>
+      </Card.Group>
+    );
+  }
 }
 
 export default NearbyCard;
--- client/src/components/cards/RecommendCard.js
+++ client/src/components/cards/RecommendCard.js
@@ -1,40 +1,40 @@
-import React, { Component } from 'react';
-import { Card, Icon, Image } from 'semantic-ui-react';
+import React, { Component } from "react";
+import { Card, Icon, Image } from "semantic-ui-react";
 
 class RecommendCard extends Component {
-
-    render() {
-        return (
-            <Card.Group className='mb-4 link' centered>
-                <Card href={this.props.url} link className='bg-transparent shadow-none mb-0' fluid>
-                    <div className='img-center-180'>
-                        <Image
-                            src={this.props.photo}
-                            wrapped
-                            ui={false}
-                        />
-                    </div>
-                {/* </Card>
-                <Card link className='bg-transparent shadow-none mt-0' fluid> */}
-                    <Card.Description textAlign='left' className='d-flex justify-content-between align-items-center pt-2 pb-0'>
-                        <h5>
-                            {this.props.title}
-                        </h5>
-                        <small>
-                            <Icon name='yellow star' />
-                            {this.props.rate}
-                        </small>
-                    </Card.Description>
-                    <Card.Description textAlign='left' className='pb-1'>
-                        กรุงเทพมหานคร
-                    </Card.Description>
-                    <Card.Description textAlign='left' className='pb-1'>
-                        {this.props.price}/ชั่วโมง
-                </Card.Description>
-                </Card>
-            </Card.Group>
-        );
-    }
+  render() {
+    return (
+      <Card.Group className="mb-4 link" centered>
+        <Card
+          href={this.props.url}
+          link
+          className="bg-transparent shadow-none mb-0 text-decoration-none"
+          fluid
+        >
+          <div className="img-center-180">
+            <Image src={this.props.photo} wrapped ui={false} />
+          </div>
+          <Card.Description
+            textAlign="left"
+            className="d-flex justify-content-between align-items-center pt-2 pb-0"
+          >
+            <h5>
+              <div>{this.props.title}</div>
+            </h5>
+            <small>
+              <Icon name="star" color="yellow" /> {this.props.rate.toFixed(1)}
+            </small>
+          </Card.Description>
+          <Card.Description textAlign="left" className="pb-1">
+            กรุงเทพมหานคร
+          </Card.Description>
+          <Card.Description textAlign="left" className="pb-1">
+            {this.props.price}/ชั่วโมง
+          </Card.Description>
+        </Card>
+      </Card.Group>
+    );
+  }
 }
 
 export default RecommendCard;
