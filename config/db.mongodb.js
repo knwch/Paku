a0eea2f5e180326ee63a114e0b99116efca63b81
famesensor <famfew25@gmail.com>
2020-04-22 01:44:14 +0700

    :label: update db

--- config/db.mongodb.js
+++ config/db.mongodb.js
@@ -1,5 +1,6 @@
 module.exports = {
-  mongoURI: `mongodb+srv://paku:${encodeURIComponent('findbyyou')}@cluster0-dj7pf.mongodb.net/paku?retryWrites=true&w=majority`,
+  // mongoURI: process.env.MONGO_URI,
+  mongoURI : `mongodb+srv://paku:${encodeURIComponent('findbyyou')}@cluster0-dj7pf.mongodb.net/paku?retryWrites=true&w=majority`,
   // mongoURI:'mongodb://localhost:27017/paku',
   secretOrKey: 'yoursecret'
 }
