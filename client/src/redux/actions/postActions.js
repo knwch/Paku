de5eeb521fd0ea7c92bcfe6d6a6e68d49881895d
famesensor <famfew25@gmail.com>
2020-04-28 17:34:40 +0700

    [update] : update try catch , change function async

--- client/src/redux/actions/postActions.js
+++ client/src/redux/actions/postActions.js
@@ -1,115 +1,107 @@
 import axios from 'axios';
 
 import {
-  ADD_POST,
-  GET_ERRORS,
-  CLEAR_ERRORS,
-  GET_POSTS,
-  GET_POST,
-  POST_LOADING,
-  DELETE_POST,
-  AVAILABLE_POST,
-  SET_POST_LOADING,
-  SET_POST_SUCCESS,
-  SEARCH_POST,
-  GET_RECOMMEND
+    ADD_POST,
+    GET_ERRORS,
+    CLEAR_ERRORS,
+    GET_POSTS,
+    GET_POST,
+    POST_LOADING,
+    DELETE_POST,
+    AVAILABLE_POST,
+    SET_POST_LOADING,
+    SET_POST_SUCCESS,
+    SEARCH_POST,
+    GET_RECOMMEND,
 } from './types';
 
 // Add Post
-export const addPost = (postData) => dispatch => {
-  dispatch(clearErrors());
-  dispatch(postStatus());
-  axios
-    .post('/api/posts/addPost', postData)
-    .then((res) =>
-      dispatch({
-        type: ADD_POST,
-        payload: res.data.post
-      }),
-      dispatch(postIssuccess())
-    )
-    .catch((err) =>
-      dispatch({
-        type: GET_ERRORS,
-        payload: err.response.data
-      })
-    );
+export const addPost = (postData) => async (dispatch) => {
+    dispatch(clearErrors());
+    dispatch(postStatus());
+    try {
+        const res = await axios.post('/api/posts/addPost', postData);
+        dispatch({
+            type: ADD_POST,
+            payload: res.data.post,
+        });
+        dispatch(postIssuccess());
+    } catch (err) {
+        dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data,
+        });
+    }
 };
 
 // Get Posts
-export const getPosts = () => dispatch => {
-  dispatch(setPostLoading());
-  axios
-    .get('/api/posts/allPost')
-    .then((res) =>
-      dispatch({
-        type: GET_POSTS,
-        payload: res.data
-      })
-    )
-    .catch((err) =>
-      dispatch({
-        type: GET_POSTS,
-        payload: null
-      })
-    );
+export const getPosts = () => async (dispatch) => {
+    dispatch(setPostLoading());
+    try {
+        const res = await axios.get('/api/posts/allPost');
+        dispatch({
+            type: GET_POSTS,
+            payload: res.data,
+        });
+    } catch (err) {
+        dispatch({
+            type: GET_POSTS,
+            payload: null,
+        });
+    }
 };
 
 // Get Post
-export const getPost = id => dispatch => {
-  dispatch(setPostLoading());
-  axios
-    .get(`/api/posts/handle/${id}`)
-    .then((res) =>
-      dispatch({
-        type: GET_POST,
-        payload: res.data
-      })
-    )
-    .catch((err) =>
-      dispatch({
-        type: GET_POST,
-        payload: null
-      })
-    );
+export const getPost = (id) => async (dispatch) => {
+    dispatch(setPostLoading());
+    try {
+        const res = await axios.get(`/api/posts/handle/${id}`);
+        dispatch({
+            type: GET_POST,
+            payload: res.data,
+        });
+    } catch (err) {
+        dispatch({
+            type: GET_POST,
+            payload: null,
+        });
+    }
 };
 
 // Edit Post
-export const editPost = (postId, postData) => dispatch => {
-  dispatch(postStatus());
-  axios
-    .post(`/api/posts/edit/${postId}`, postData)
-    .then((res) => 
-      dispatch({
-        type: GET_POST,
-        payload: res.data
-      }),
-      dispatch(postIssuccess())
-    )
-    .catch((err) => {
-      dispatch({
-        type: GET_ERRORS,
-        payload: err.response.data
-      })
-    })
+export const editPost = (postId, postData) => async (dispatch) => {
+    dispatch(postStatus());
+    try {
+        const res = await axios.post(`/api/posts/edit/${postId}`, postData);
+        dispatch({
+            type: GET_POST,
+            payload: res.data,
+        });
+        dispatch(postIssuccess());
+    } catch (err) {
+        dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data,
+        });
+    }
 };
 
 // Delete Post
-export const deletePost = id => dispatch => {
-  axios
-    .delete(`/api/posts/delete/${id}`)
-    .then((res) =>
-      dispatch({
-        type: DELETE_POST,
-        payload: id
-      })
-    )
-    .catch((err) =>
-      dispatch({
-        type: GET_ERRORS,
-        payload: err.response.data
-      })
-    );
+export const deletePost = (id) => (dispatch) => {
+    axios
+        .delete(`/api/posts/delete/${id}`)
+        .then((res) =>
+            dispatch({
+                type: DELETE_POST,
+                payload: id,
+            })
+        )
+        .catch((err) =>
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data,
+            })
+        );
 };
 
 // // Add Like
@@ -139,22 +131,22 @@ export const deletePost = id => dispatch => {
 // };
 
 // Add Comment
-export const addComment = (postId, commentData) => dispatch => {
-  dispatch(clearErrors());
-  axios
-    .post(`/api/posts/comment/${postId}`, commentData)
-    .then((res) =>
-      dispatch({
-        type: GET_POST,
-        payload: res.data
-      })
-    )
-    .catch((err) =>
-      dispatch({
-        type: GET_ERRORS,
-        payload: err.response.data
-      })
-    );
+export const addComment = (postId, commentData) => (dispatch) => {
+    dispatch(clearErrors());
+    axios
+        .post(`/api/posts/comment/${postId}`, commentData)
+        .then((res) =>
+            dispatch({
+                type: GET_POST,
+                payload: res.data,
+            })
+        )
+        .catch((err) =>
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data,
+            })
+        );
 };
 
 // // Delete Comment
@@ -176,87 +168,86 @@ export const addComment = (postId, commentData) => dispatch => {
 // };
 
 // Available post
-export const availablePost = (availableData, idPost) => dispatch => {
-  dispatch(setPostLoading())
-  axios.post(`/api/posts/available/${idPost}`, availableData)
-    .then((res) => {
-      dispatch({
-        type: DELETE_POST,
-        payload: res.data._id
-      })
-      dispatch({
-        type: AVAILABLE_POST,
-        payload: res.data
-      })
-    })
-    .catch((err) => {
-      dispatch({
-        type: GET_ERRORS,
-        payload: err.response.data
-      })
-    })
-}
+export const availablePost = (availableData, idPost) => (dispatch) => {
+    dispatch(setPostLoading());
+    axios
+        .post(`/api/posts/available/${idPost}`, availableData)
+        .then((res) => {
+            dispatch({
+                type: DELETE_POST,
+                payload: res.data._id,
+            });
+            dispatch({
+                type: AVAILABLE_POST,
+                payload: res.data,
+            });
+        })
+        .catch((err) => {
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data,
+            });
+        });
+};
 
-// Search post 
-export const searchPost = (search) => dispatch => {
-  // dispatch(setPostLoading())
-  axios.get(`/api/posts/search?q=${search}`)  
-    .then((res) => {
-      dispatch({
-        type: SEARCH_POST,
-        payload: res.data
-      })
-    })
-    .catch((err) => {
-      dispatch({
-        type: SEARCH_POST,
-        payload: null
-      })
-    })
-}
+// Search post
+export const searchPost = (search) => async (dispatch) => {
+    // dispatch(setPostLoading())
+    try {
+        const res = await axios.get(`/api/posts/search?q=${search}`);
+        dispatch({
+            type: SEARCH_POST,
+            payload: res.data,
+        });
+    } catch (err) {
+        dispatch({
+            type: SEARCH_POST,
+            payload: null,
+        });
+    }
+};
 
 // Get recommend
-export const recommendPost = () => dispatch => {
-  dispatch(setPostLoading())
-  axios.get('/api/posts/recommend')
-    .then((res) => {
-      dispatch({
-        type: GET_RECOMMEND,
-        payload: res.data
-      })
-    })
-    .catch((err) => {
-      dispatch({
-        type: GET_ERRORS,
-        payload: err.response.data
-      })
-    })
-}
+export const recommendPost = () => async (dispatch) => {
+    dispatch(setPostLoading());
+    try {
+        const res = await axios.get('/api/posts/recommend');
+        dispatch({
+            type: GET_RECOMMEND,
+            payload: res.data,
+        });
+    } catch (err) {
+        dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data,
+        });
+    }
+};
 
 // Set loading state
 export const setPostLoading = () => {
-  return {
-    type: POST_LOADING
-  };
+    return {
+        type: POST_LOADING,
+    };
 };
 
 // Clear errors
 export const clearErrors = () => {
-  return {
-    type: CLEAR_ERRORS
-  };
+    return {
+        type: CLEAR_ERRORS,
+    };
 };
 
 // Set loading state
 export const postStatus = () => {
-  return {
-    type: SET_POST_LOADING
-  }
-}
+    return {
+        type: SET_POST_LOADING,
+    };
+};
 
 // Result status
 export const postIssuccess = () => {
-  return {
-    type: SET_POST_SUCCESS
-  } 
-}
+    return {
+        type: SET_POST_SUCCESS,
+    };
+};
