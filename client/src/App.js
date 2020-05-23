d32e58a9897087d9b9df113d06c99c24ccb005c5
famesensor <famfew25@gmail.com>
2020-05-23 16:34:38 +0700

    [update] sturcture

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import {
     BrowserRouter as Router,
     Route,
     Switch,
-    Redirect,
+    Redirect
 } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
@@ -32,8 +32,6 @@ import PostDetail from './components/pages/PostDetail';
 import EditPost from './components/pages/EditPost';
 import Admin from './components/admin/Admin';
 import VerifyUser from './components/admin/VerifyUser';
-let axiosDefaults = require('axios/lib/defaults');
-axiosDefaults.baseURL = 'https://paku-official.herokuapp.com/';
 
 // Check for token
 if (localStorage.jwtToken) {
@@ -61,53 +59,53 @@ class App extends Component {
         return (
             <Provider store={store}>
                 <Router>
-                    <div className="App">
+                    <div className='App'>
                         <Switch>
-                            <Route exact path="/" component={Home} />
-                            <Route exact path="/login" component={Login} />
+                            <Route exact path='/' component={Home} />
+                            <Route exact path='/login' component={Login} />
                             <Route
                                 exact
-                                path="/register"
+                                path='/register'
                                 component={Register}
                             />
-                            <PrivateRoute exact path="/post" component={Post} />
-                            <Route exact path="/rent" component={Rent} />
-                            <Route exact path="/book/:id" component={Book} />
-                            <Route exact path="/owner" component={Owner} />
+                            <PrivateRoute exact path='/post' component={Post} />
+                            <Route exact path='/rent' component={Rent} />
+                            <Route exact path='/book/:id' component={Book} />
+                            <Route exact path='/owner' component={Owner} />
                             <PrivateRoute
                                 exact
-                                path="/profile"
+                                path='/profile'
                                 component={Profile}
                             />
-                            <Route exact path="/support" component={Support} />
+                            <Route exact path='/support' component={Support} />
                             <PrivateRoute
                                 exact
-                                path="/confirmcard"
+                                path='/confirmcard'
                                 component={ConfirmCard}
                             />
                             <PrivateRoute
                                 exact
-                                path="/mypost"
+                                path='/mypost'
                                 component={MyPost}
                             />
                             <PrivateRoute
                                 exact
-                                path="/post/:id"
+                                path='/post/:id'
                                 component={PostDetail}
                             />
                             <PrivateRoute
                                 exact
-                                path="/editpost/:id"
+                                path='/editpost/:id'
                                 component={EditPost}
                             />
-                            <AdminRoute exact path="/admin" component={Admin} />
+                            <AdminRoute exact path='/admin' component={Admin} />
                             <AdminRoute
                                 exact
-                                path="/verifyuser"
+                                path='/verifyuser'
                                 component={VerifyUser}
                             />
-                            <Route path="*">
-                                <Redirect to="/" />
+                            <Route path='*'>
+                                <Redirect to='/' />
                             </Route>
                         </Switch>
                     </div>
