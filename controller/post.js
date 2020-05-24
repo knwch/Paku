cc5d6372834508ee9fce5b09910b6e2d1a2867d1
famesensor <famfew25@gmail.com>
2020-05-24 22:40:15 +0700

    :sparkles: new function rencommed

--- controller/post.js
+++ controller/post.js
@@ -1,20 +1,29 @@
-const Post = require('../models/post')
+const Post = require('../models/post');
 
 exports.search = async (req, res) => {
-    const query = req.query.q
+    const query = req.query.q;
     if (query) {
         const post = await Post.find({
             $and: [
                 {
                     $or: [
                         {
-                            title: { $regex: '.*' + query + '.*', $options: 'i' }
+                            title: {
+                                $regex: '.*' + query + '.*',
+                                $options: 'i'
+                            }
                         },
                         {
-                            'detail.nearby': { $regex: '.*' + query + '.*', $options: 'i' }        
+                            'detail.nearby': {
+                                $regex: '.*' + query + '.*',
+                                $options: 'i'
+                            }
                         },
                         {
-                            'location.address': { $regex: '.*' + query + '.*', $options: 'i' }
+                            'location.address': {
+                                $regex: '.*' + query + '.*',
+                                $options: 'i'
+                            }
                         }
                     ]
                 },
@@ -23,24 +32,30 @@ exports.search = async (req, res) => {
                 }
             ]
         })
-        .select({ comments: 0, available: 0, detail: 0, created: 0, __v: 0})
-        .sort({ 'rate.rating': -1 })
+            .select({
+                comments: 0,
+                available: 0,
+                detail: 0,
+                created: 0,
+                __v: 0
+            })
+            .sort({ 'rate.rating': -1 });
 
         if (post.length === 0) {
-            return res.status(200).json({ post: 'No have post' })
+            return res.status(200).json({ post: 'No have post' });
         }
 
-        res.status(200).json(post)
+        res.status(200).json(post);
     } else {
-        const posts = await Post.find().sort({ created: -1 })
-        
+        const posts = await Post.find().sort({ created: -1 });
+
         if (posts.length === 0) {
-            return res.status(200).json({ post: 'No have post' })
+            return res.status(200).json({ post: 'No have post' });
         }
 
-        res.status(200).json(posts)
+        res.status(200).json(posts);
     }
-}
+};
 
 exports.recommend = async (req, res) => {
     const posts = await Post.find({
@@ -52,12 +67,22 @@ exports.recommend = async (req, res) => {
                 available: true
             }
         ]
-    }).select({ comments: 0, available: 0, detail: 0, created: 0, __v: 0})
-    .limit(8).sort({ created: -1 })
+    })
+        .select({ comments: 0, available: 0, detail: 0, created: 0, __v: 0 })
+        .sort({ created: -1 });
 
-    if (posts.length === 0) {
-        return res.status(200).json({ post: 'No have post' })
+    let temp = [];
+    let result = [];
+    for (i = 0; i < posts.length; i++) {
+        let random = Math.floor(Math.random() * posts.length);
+        if (!temp.some((temp) => temp === random)) {
+            result.push(posts[random]);
+        } else {
+            continue;
+        }
+        temp.push(random);
+        if (result.length === 8) break;
     }
 
-    res.status(200).json(posts)
-}
+    res.status(200).json(result);
+};
